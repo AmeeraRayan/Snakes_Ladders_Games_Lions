4a2d330f740d9fdb779a7406454597ef73911a6b
AmeeraRayan <ameerarayan2@gmail.com>
2024-02-04 01:24:41 +0200

    Update BoardEasyView2Players.java

--- src/View/BoardEasyView2Players.java
+++ src/View/BoardEasyView2Players.java
@@ -310,9 +310,14 @@ public class BoardEasyView2Players extends JFrame {
 	    	        System.out.println(quesTemp);
 	                new QuestionpopUP(quesTemp , player,game).setVisible(true);
 	                break;
+<<<<<<< Updated upstream
 
 	            }
 	        }
+=======
+	            }
+	            }
+>>>>>>> Stashed changes
 	        
 	    }
 	    private void displayCurrentPlayer() {
