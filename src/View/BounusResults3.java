b8dceb4361e7a190ce4bbfabe322a5d906f82b3c
khaldyAseel <132208072+khaldyAseel@users.noreply.github.com>
2024-03-02 10:54:43 +0200

    bonus screens

--- src/View/BounusResults3.java
+++ src/View/BounusResults3.java
@@ -25,33 +25,32 @@ public class BounusResults3 extends JFrame {
 
 	public BounusResults3( String difficultyLevel,List<Player>  playersSortedByOrder) {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 750, 590);
+		setBounds(100, 100, 890, 637);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
-		JLabel lblNewLabel_1 = new JLabel("Player's Turn: ");
-		lblNewLabel_1.setForeground(Color.WHITE);
-		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 36));
-		lblNewLabel_1.setBounds(101, 31, 354, 52);
-		contentPane.add(lblNewLabel_1);
+		 // Make the frame undecorated (no title bar, no minimize/maximize/close buttons)
+        setUndecorated(true);
 		
 		JLabel Player3 = new JLabel(" "+playersSortedByOrder.get(2).getName());
-		Player3.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 30));
-		Player3.setBounds(228, 402, 274, 72);
+		Player3.setForeground(new Color(0, 0, 0));
+		Player3.setFont(new Font("Tahoma", Font.BOLD, 30));
+		Player3.setBounds(379, 452, 274, 72);
 		contentPane.add(Player3);
 		
 		JLabel Player2 = new JLabel(" "+playersSortedByOrder.get(1).getName());
-		Player2.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 30));
-		Player2.setBounds(235, 263, 246, 72);
+		Player2.setForeground(new Color(0, 0, 0));
+		Player2.setFont(new Font("Tahoma", Font.BOLD, 30));
+		Player2.setBounds(379, 319, 246, 72);
 		contentPane.add(Player2);
 		
 		JLabel Player1 = new JLabel(" " + playersSortedByOrder.get(0).getName());
-		Player1.setBounds(240, 118, 262, 72);
+		Player1.setForeground(new Color(0, 0, 0));
+		Player1.setBounds(379, 180, 262, 72);
 		contentPane.add(Player1);
-		Player1.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 30));
+		Player1.setFont(new Font("Tahoma", Font.BOLD, 30));
        
 		JButton btnNewButton_1 = new JButton("Next");
         btnNewButton_1.addActionListener(new ActionListener() {
@@ -72,12 +71,12 @@ public class BounusResults3 extends JFrame {
                            }
             }
         });
-        btnNewButton_1.setBounds(556, 434, 105, 34);
+        btnNewButton_1.setBounds(760, 575, 105, 34);
         contentPane.add(btnNewButton_1);
 		
 		JLabel lblNewLabel = new JLabel("");
-		lblNewLabel.setIcon(new ImageIcon(BounusResults3.class.getResource("/images/Bounus 11 (1).png")));
-		lblNewLabel.setBounds(-159, 0, 1106, 640);
+		lblNewLabel.setIcon(new ImageIcon(BounusResults3.class.getResource("/images/bonus3.jpg")));
+		lblNewLabel.setBounds(-22, 0, 1207, 651);
 		contentPane.add(lblNewLabel);
 	}
 }
